dea78021271bb61f720f63f9ba3829432328bd8f
Jan Krcmar <honza801@gmail.com>
2010-09-28 15:31:47 +0200

    new features
    filechooser ignores filenames with bad characters (ord > 128)
    
    tv script uses cvlc with ncurses interface

--- filechooser.py
+++ filechooser.py
@@ -6,7 +6,11 @@ import string
 class FileChooser():
 
 	def __init__(self):
-		self.allowed_prefix = [ '/mnt/data', '/home/ftp/serialy' ]
+		self.allowed_prefix = [ 
+			'/mnt/data',
+			'/home/ftp/serialy',
+			'/mnt/kesunka/serialy',
+			]
 		self.cpath = self.allowed_prefix[0] 
 	
 	def getAllowed(self):
@@ -32,10 +36,17 @@ class FileChooser():
 			return False
 	
 	def getList(self):
-		out = os.listdir(self.cpath)
-		out.sort()
+		out = self.transformList(os.listdir(self.cpath))
 		return out
 	
+	def transformList(self, list):
+		out = []
+		list.sort()
+		for d in list:
+			if all(ord(c) < 128 for c in d):
+				out.append(d)
+		return out
+
 		
 if __name__ == "__main__":
 	fc = FileChooser()
